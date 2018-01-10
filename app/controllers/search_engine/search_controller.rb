3576cdb16ced60302421d25004da5a247801c9fb
dasibre <james.naadjie@gmail.com>
2018-01-10 10:44:58 -0500

    Add controller specs with support

--- app/controllers/search_engine/search_controller.rb
+++ app/controllers/search_engine/search_controller.rb
@@ -2,5 +2,15 @@ require_dependency "search_engine/application_controller"
 
 module SearchEngine
   class SearchController < ApplicationController
+
+    def new
+      @results = client.query(params)
+      render json: {}, status: 200
+    end
+
+    private
+
+    def client
+    end
   end
 end
--- spec/controllers/search_engine/search_controller_spec.rb
+++ spec/controllers/search_engine/search_controller_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 module SearchEngine
   RSpec.describe SearchController, type: :controller do
-
+    describe 'Get new' do
+      it "runs search for given term" do
+        search_client = double('SearchClient')
+        allow_any_instance_of(SearchController).to receive(:client).and_return(search_client)
+        get :new, { search_term: 'foobar' }
+        expect(search_client).to receive(:query).with('foobar')
+      end
+    end
   end
 end
