6af74bc5fed3daf7de39fc83fe21046e623495d5
dasibre <james.naadjie@gmail.com>
2018-01-11 08:07:31 -0500

    add view rendered spec example

--- spec/controllers/search_engine/search_controller_spec.rb
+++ spec/controllers/search_engine/search_controller_spec.rb
@@ -2,12 +2,34 @@ require 'rails_helper'
 
 module SearchEngine
   RSpec.describe SearchController, type: :controller do
+    include Engine.routes.url_helpers
+
+    before do
+      @routes = Engine.routes
+    end
+
     describe 'Get new' do
-      it "runs search for given term" do
+      it "runs query using search client" do
         search_client = double('SearchClient')
         allow_any_instance_of(SearchController).to receive(:client).and_return(search_client)
-        get :new, { search_term: 'foobar' }
         expect(search_client).to receive(:query).with('foobar')
+        get :new, search_term: "foobar"
+      end
+
+      it "renders show template view" do
+        search_client = double('SearchClient')
+        allow_any_instance_of(SearchController).to receive(:client).and_return(search_client)
+        allow(search_client).to receive(:query).with('foobar').and_return(%w(doc doc2))
+        get :new, search_term: "foobar"
+        expect(response).to render_template(:show)
+      end
+
+      it "assigns @results" do
+        search_client = double('SearchClient')
+        allow_any_instance_of(SearchController).to receive(:client).and_return(search_client)
+        allow(search_client).to receive(:query).with('foobar').and_return(%w(doc doc2))
+        get :new, search_term: "foobar"
+        expect(assigns(:results)).to eq(['doc', 'doc2'])
       end
     end
   end
