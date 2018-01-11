7e40b12d339c51283de5a4b46fb82c6ede62cf14
dasibre <james.naadjie@gmail.com>
2018-01-11 08:06:41 -0500

    render show when new action is called

--- app/controllers/search_engine/search_controller.rb
+++ app/controllers/search_engine/search_controller.rb
@@ -4,13 +4,20 @@ module SearchEngine
   class SearchController < ApplicationController
 
     def new
-      @results = client.query(params)
-      render json: {}, status: 200
+      @results = client.query(params[:search_term])
+      render :show
     end
 
     private
 
     def client
+      @client ||= Struct.new(:name, :address) do
+        def query(term)
+          ["Hello #{term}!", "result"]
+        end
+      end.new
     end
+
+
   end
 end
