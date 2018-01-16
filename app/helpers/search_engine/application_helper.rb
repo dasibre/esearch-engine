5d93a4960bac64967ff4835d3ed779ac8b4f1877
dasibre <james.naadjie@gmail.com>
2018-01-16 15:38:21 -0500

    refactor gem, move helper method to engine application helper, to allow host app access

--- app/helpers/search_engine/application_helper.rb
+++ app/helpers/search_engine/application_helper.rb
@@ -1,4 +1,7 @@
 module SearchEngine
   module ApplicationHelper
+    def search_form
+      "search_engine/search/search_form"
+    end
   end
 end
--- lib/search_engine.rb
+++ lib/search_engine.rb
@@ -2,7 +2,4 @@ require "search_engine/engine"
 require "elasticsearch"
 
 module SearchEngine
-  def self.search_form
-    "search_engine/search/search_form"
-  end
 end
